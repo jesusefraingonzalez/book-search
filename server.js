aa421ef81feeb9c50e25af09ab0439dedfc6cbcb
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-23 03:12:09 -0400

    add routes to express app

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books", {
 	useNewUrlParser: true,
