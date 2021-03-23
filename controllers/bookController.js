de9b012b1c19a7ca1eac25c23fb557a9647df697
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-23 02:50:27 -0400

    add functionality to controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,15 +1,26 @@
 const { Book } = require("../models");
+const { deleteOne } = require("../models/book");
 
 module.exports = {
 	findAll: (req, res) => {
-
+		Book
+			.find(req.query)
+			.then(model => res.json(model))
+			.catch(err => res.status(422).json(err));
 	},
 
 	create: (req, res) => {
-
+		Book
+			.create(req.body)
+			.then(model => res.json(model))
+			.catch(err => res.status(422).json(err));
 	},
 
 	delete: (req, res) => {
-
+		Book
+			.findById({ _id: req.params.id })
+			.then(model => deleteOne(model))
+			.then(model => res.json(model))
+			.catch(err => res.status(422).json(err));
 	},
 }
