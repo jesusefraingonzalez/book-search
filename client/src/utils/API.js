811effa0f546d3178b2bd8cfd18aa803da4dc78e
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-23 22:54:47 -0400

    add googlebooks api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export default {
 	getBooks: () => axios.get("/api/books"),
 	deleteBook: (id) => axios.delete(`/api/books/${id}`),
-	saveBook: (data) => axios.post(`/api/books/`, data)
+	saveBook: (data) => axios.post(`/api/books/`, data),
+	getGoogleBooks: (query) => axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`), 
 }
