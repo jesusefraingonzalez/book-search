e2683faa0555c8ed5b2268686e3dd44eaad76190
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-23 22:53:47 -0400

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import Search from "./pages/Search";
+import SavedBooks from "./pages/SavedBooks";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-
+import {Container} from "@material-ui/core";
+import { Header } from "./components";
 function App() {
   return (
-    <Search></Search>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path={["/", "index.html", "search.html"]}>
+          <Container>
+            <h1>Search Page</h1>
+          </Container>
+        </Route>
+        <Route exact path={["saved-books"]}>
+          <Container>
+            <SavedBooks />
+          </Container>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
